72ae699d0e62768beaa01cc61d468e743dbab068
dustin <58792893+dshook420@users.noreply.github.com>
2023-07-28 13:37:14 -0400

    form validation

--- js/main.js
+++ js/main.js
@@ -12,42 +12,51 @@ const hideCustomError = ({ id }) => {
     span.classList.remove('active');
 };
 
+const handleBlur = ({ target }) => {
+    if (target.validity.valid || target.value === '') {
+        target.classList.remove('invalid');
+        hideCustomError(target);
+    }
+};
+
+const handleInput = ({ target }) => {
+    if (!target.validity.typeMismatch && !target.validity.patternMismatch) {
+        target.classList.remove('invalid');
+        hideCustomError(target);
+        target.setCustomValidity('');
+    } else {
+        target.classList.add('invalid');
+        target.setCustomValidity(`* please enter a valid ${target.id}`);
+        displayCustomError(target);
+    }
+};
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const inputs = document.querySelectorAll('input');
     const password = document.getElementById('password');
 
     inputs.forEach(input => {
         if (!input.id.includes('confirm')) {
+            input.addEventListener('input', handleInput);
+            input.addEventListener('blur', handleBlur);
+        } else {
             input.addEventListener('input', () => {
-                if (input.validity.valid) {
+                if (input.value === password.value) {
                     input.classList.remove('invalid');
+                    input.setCustomValidity('');
                     hideCustomError(input);
                 } else {
                     input.classList.add('invalid');
-                    input.setCustomValidity('* please enter a valid email');
+                    input.setCustomValidity('Passwords do not match');
                     displayCustomError(input);
                 }
             });
 
             input.addEventListener('blur', () => {
-                if (input.validity.valid || input.value === '') {
+                if (input.value === password.value) {
                     input.classList.remove('invalid');
                     hideCustomError(input);
-                }
-            });
-        } else {
-            input.addEventListener('input', () => {
-                if (input.value === password.value) {
-                    input.classList.remove('invalid');
-                } else {
-                    input.classList.add('invalid');
-                    input.setCustomValidity('Passwords do not match');
-                }
-            });
-
-            input.addEventListener('blur', () => {
-                if (input.value === password.value) {
-                    input.classList.remove('invalid');
                 }
             });
 
